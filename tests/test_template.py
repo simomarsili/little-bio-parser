bf659e07e7114fed5ef8668d928242576c853ca1
simomarsili <simomarsili@gmail.com>
2019-01-18 13:08:34 +0100

    update tests

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,4 +1,4 @@
-RECORDS = "[('VP54_CPSVP/90-227', '........LGTLKSITDKLRKLGGESSQPFIQFYKVQCMYIPLFSRVDGDNG......EITVSLIDDGKEAAGQDPIIQSITFDASQMAMVELSMNFFVEKKDMDFIGIHVSAENVPVQDRAYGSINLAFFTNEQSVPMMQEEKKSSYLMID..'), ('Q6E6Y4_9VIRU/93-231', 'ttkvkitt-------MDKVTSLIKFEKFPFYRVDRLKILYIPLFSGENSEGK......NITFSIQDRSMVVAGKPKKISSATAPINKMSMIELSATYFVQSKDLSKIEFGYKAKGIPVSGRSFAAVYLAFYIHGDHFPATMRPKDPIVLLID..'), ('A0A075IE34_9VIRU/105-247', '.iatmgrv-------VNLFKKATG-NEMPFVKFEKVQVMYIPLFQKTNEEDDpdkkipSMTVALVDKGQEEAGGDGIIQSITFRADEMALMELSMNFFVTRKDIEKIVVDACVDEIPVEGRAYGAMTIAFFVHEDYVPLRTELKPSTLMY--it')]"
+RECORDS = "[(0, 'VP54_CPSVP/90-227', '........LGTLKSITDKLRKLGGESSQPFIQFYKVQCMYIPLFSRVDGDNG......EITVSLIDDGKEAAGQDPIIQSITFDASQMAMVELSMNFFVEKKDMDFIGIHVSAENVPVQDRAYGSINLAFFTNEQSVPMMQEEKKSSYLMID..'), (1, 'Q6E6Y4_9VIRU/93-231', 'ttkvkitt-------MDKVTSLIKFEKFPFYRVDRLKILYIPLFSGENSEGK......NITFSIQDRSMVVAGKPKKISSATAPINKMSMIELSATYFVQSKDLSKIEFGYKAKGIPVSGRSFAAVYLAFYIHGDHFPATMRPKDPIVLLID..'), (2, 'A0A075IE34_9VIRU/105-247', '.iatmgrv-------VNLFKKATG-NEMPFVKFEKVQVMYIPLFQKTNEEDDpdkkipSMTVALVDKGQEEAGGDGIIQSITFRADEMALMELSMNFFVTRKDIEKIVVDACVDEIPVEGRAYGAMTIAFFVHEDYVPLRTELKPSTLMY--it')]"
 
 
 def tests_dir():
@@ -20,17 +20,15 @@ def test_0():
 
 def test_fasta():
     import os
-    from gopen import gread
-    from lilbio.bioparsers import fasta_parser as parser
+    import lilbio
     fname = os.path.join(tests_dir(), '1.fa')
-    a = [record for record in parser(gread(fname))]
+    a = [record for record in lilbio.parse(fname, 'fasta')]
     assert repr(a) == RECORDS
 
 
 def test_stockholm():
     import os
-    from gopen import gread
-    from lilbio.bioparsers import stockholm_parser as parser
+    import lilbio
     fname = os.path.join(tests_dir(), '1.sto')
-    a = [record for record in parser(gread(fname))]
+    a = [record for record in lilbio.parse(fname, 'stockholm')]
     assert repr(a) == RECORDS
