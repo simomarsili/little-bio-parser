7875f37d7be38726ae03bb37b04cff9058b440e2
simomarsili <simomarsili@gmail.com>
2019-01-18 13:09:46 +0100

    update tests

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,9 +1,11 @@
+import os
+import lilbio
+
 RECORDS = "[(0, 'VP54_CPSVP/90-227', '........LGTLKSITDKLRKLGGESSQPFIQFYKVQCMYIPLFSRVDGDNG......EITVSLIDDGKEAAGQDPIIQSITFDASQMAMVELSMNFFVEKKDMDFIGIHVSAENVPVQDRAYGSINLAFFTNEQSVPMMQEEKKSSYLMID..'), (1, 'Q6E6Y4_9VIRU/93-231', 'ttkvkitt-------MDKVTSLIKFEKFPFYRVDRLKILYIPLFSGENSEGK......NITFSIQDRSMVVAGKPKKISSATAPINKMSMIELSATYFVQSKDLSKIEFGYKAKGIPVSGRSFAAVYLAFYIHGDHFPATMRPKDPIVLLID..'), (2, 'A0A075IE34_9VIRU/105-247', '.iatmgrv-------VNLFKKATG-NEMPFVKFEKVQVMYIPLFQKTNEEDDpdkkipSMTVALVDKGQEEAGGDGIIQSITFRADEMALMELSMNFFVTRKDIEKIVVDACVDEIPVEGRAYGAMTIAFFVHEDYVPLRTELKPSTLMY--it')]"
 
 
 def tests_dir():
     """Return None is no tests dir."""
-    import os
     cwd = os.getcwd()
     basename = os.path.basename(cwd)
     if basename == 'tests':
@@ -19,16 +21,12 @@ def test_0():
 
 
 def test_fasta():
-    import os
-    import lilbio
     fname = os.path.join(tests_dir(), '1.fa')
-    a = [record for record in lilbio.parse(fname, 'fasta')]
+    a = list(lilbio.parse(fname, 'fasta'))
     assert repr(a) == RECORDS
 
 
 def test_stockholm():
-    import os
-    import lilbio
     fname = os.path.join(tests_dir(), '1.sto')
-    a = [record for record in lilbio.parse(fname, 'stockholm')]
+    a = list(lilbio.parse(fname, 'stockholm'))
     assert repr(a) == RECORDS
