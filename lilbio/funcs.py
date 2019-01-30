a0e2b853e24adc4aa1f3b87bbf6a54a450ae5877
simomarsili <simomarsili@gmail.com>
2019-01-30 17:07:03 +0100

    prepare merge with develop

--- lilbio/funcs.py
+++ lilbio/funcs.py
@@ -1,7 +1,11 @@
-"""Filter/transform records."""
+"""
+Functions of sequences. If a transformed version of the sequence is
+returned, it should be a list of symbols (not a string).
+"""
 import functools
-import numpy
+import logging
 
+logger = logging.getLogger(__name__)
 # uppercase plus *,- symbols
 GENERIC_ALPHABET = set('*-ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 # Canonical/natural codes plus gap symbol.
@@ -9,44 +13,19 @@ PROTEIN_ALPHABET = set('-ACDEFGHIKLMNPQRSTVWY')
 
 
 def compose(*funcs):
-    # compose a list of functions (right to left)
-    def fg(f, g):
-        return lambda recs: f(g(recs))
-    return functools.reduce(fg, funcs, lambda recs: recs)
+    """Compose an iterable of functions.
+
+    Functions operate in the list order:
+    compose([a, b])(x) == b(a(x)).
+    """
+    def gf(f, g):
+        return lambda x: g(f(x))
+    return functools.reduce(gf, funcs, lambda x: x)
 
 
-def as_list(a):
-    for k, head, seq in a:
-        yield k, head, list(seq)
+def in_alphabet(a, alphabet):
+    return [c for c in a if c in alphabet]
 
 
-def as_array(a):
-    for k, head, seq in a:
-        yield k, head, numpy.array(list(seq))
-
-
-def hmm_positions(a):
-    for k, head, seq in a:
-        seq = ''.join([c for c in seq if c in GENERIC_ALPHABET])
-        yield k, head, seq
-
-
-def hmm_positions2(a):
-    inds = None
-
-    # a = as_array(a)
-
-    def wrapped():
-        nonlocal inds
-        if inds is None:
-            k, head, seq = next(a)
-            seq = numpy.array(list(seq))
-            inds = [j for j, c in enumerate(seq) if c in GENERIC_ALPHABET]
-            seq = seq[inds]
-            yield k, head, seq
-        for k, head, seq in a:
-            seq = numpy.array(list(seq))
-            seq = seq[inds]
-            yield k, head, seq
-
-    return wrapped()
+def hmm(a):
+    return in_alphabet(a, GENERIC_ALPHABET)
