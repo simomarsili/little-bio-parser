eaaecf89861c33b4c40d377af12854ab0577319b
simomarsili <simomarsili@gmail.com>
2019-02-13 17:01:04 +0100

    rm logging basic config

--- lilbio/utils.py
+++ lilbio/utils.py
@@ -61,5 +61,5 @@ def config_loggers():
         format='%(module)-10s %(funcName)-20s: %(levelname)-8s %(message)s',
         datefmt='%H:%M:%S',
         # level=logging.DEBUG)
-        level=logging.WARNING)
+        level=logging.DEBUG)
     # add_notes_handler()
